886ab9abd67d387e392e3a47faf0fc1feb7fdf1c
Doru Floare <dorufloare@yahoo.com>
2024-09-08 22:35:18 +0300

    Added colisions

--- main.py
+++ main.py
@@ -1,46 +1,39 @@
 import pygame
+from lib.controls import *
 from lib.globals import *
 from lib.car import Car
+from lib.utils import *
 
 def main():
-    pygame.init()
-    pygame.mixer.init()
-    window = pygame.display.set_mode((WINDOW_WIDTH, WINDOW_HEIGHT))
-    pygame.display.set_caption("Car Steering")
-
-    car_image = pygame.image.load(CAR_IMAGE_PATH).convert_alpha()
-    car = Car(car_image, WINDOW_WIDTH // 2, WINDOW_HEIGHT // 2)
+    window, game_map = initialize_pygame()
+    car = Car(CAR_START_X, CAR_START_Y)
     cars = pygame.sprite.Group()
     cars.add(car)
 
     clock = pygame.time.Clock()
     done = False
+
     while not done:
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
                 done = True
 
         keys = pygame.key.get_pressed()
-        if keys[pygame.K_w]:
-            car.accelerate()
-        if keys[pygame.K_s]:
-            car.brake()
-        if keys[pygame.K_a]:
-            car.left_turn()
-        if keys[pygame.K_d]:
-            car.right_turn()
 
-        cars.update()
+        handle_user_controls(car, keys)
+        handle_game_controls(keys);
+   
+        cars.update(game_map)
 
-        window.fill((0, 0, 0))
+        window.blit(game_map, (0, 0))
         cars.draw(window)
-        for corner in car.get_corners():
-            pygame.draw.circle(window, (255, 255, 0), (int(corner[0]), int(corner[1])), 5)
+       
         pygame.display.flip()
 
         clock.tick_busy_loop(60)
 
     pygame.quit()
+
 
 if __name__ == "__main__":
     main()
